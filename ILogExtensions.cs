273137128f63dd61adee25f5fb18e5fb6468fb98
vinnylinck <vinnylinck@outlook.com>
2015-02-16 09:18:52 -0200

    Initial commit.

---
+++ ILogExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace log4net {
+	namespace Ext {
+
+		public static class ILogExtentions 
+		{
+			//private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+			public static void Trace(this ILog log, string message, Exception exception)
+			{
+				log.Logger.Log(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType,log4net.Core.Level.Trace, message, exception);
+			}
+
+			public static void Trace(this ILog log, string message)
+			{
+				log.Trace(message, null);
+			}
+		}
+	}
+}
---
+++ SocketAppender.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+
+using System.Net; 
+using System.Net.Sockets; 
+
+using log4net.Core;
+using log4net.Appender;
+
+namespace log4net
+{
+	namespace Appender {
+
+		public class SocketAppender : AppenderSkeleton
+		{
+			// properties set by log4net xml config file
+			public string remoteAddress { get; set;}
+			public int    remotePort { get; set;}
+			public bool   debugMode { get; set;}
+
+			// this is the socket (you don't say!)
+			private Socket sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+			// connect to a remote device
+			public override void ActivateOptions() {
+
+				// Establish the remote endpoint for the socket.
+				try {
+					sender.Connect (remoteAddress, remotePort);
+				} catch (ArgumentNullException ane) {
+					Console.WriteLine("ArgumentNullException : {0}",ane.ToString());
+
+				} catch (SocketException se) {
+					Console.WriteLine("SocketException : {0}",se.ToString());
+
+				} catch (Exception e) {
+					Console.WriteLine("Unexpected exception : {0}", e.ToString());
+				}
+
+			}
+
+			protected override void Append(LoggingEvent loggingEvent) {
+				string rendered = "";
+
+				// check connectivity before send
+				if (sender.Connected) {
+
+					// Renderizing event
+					rendered = RenderLoggingEvent (loggingEvent);
+
+					// Encode the data string into a byte array.
+					byte[] msg = Encoding.UTF8.GetBytes(rendered);
+
+					// Send the data through the socket.
+					int bytesSent = sender.Send(msg);
+
+					if (debugMode) {
+						Console.WriteLine ("- Bytes sent: " + bytesSent.ToString());
+					}
+				} else {
+					// accumulate?!
+					Console.WriteLine ("[SKIPPED]:: " + rendered);
+				}
+			}
+
+			protected override void OnClose() {
+				sender.Shutdown (SocketShutdown.Both);
+				sender.Close ();
+			}
+		}
+
+	}
+}
